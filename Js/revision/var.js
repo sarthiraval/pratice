cdfc29a88ce95e6f7315d391d13933f06e63277f
sarthi raval <ser20810@gmail.com>
2022-12-29 22:20:14 +0530

    His

--- Js/revision/var.js
+++ Js/revision/var.js
@@ -337,3 +337,211 @@ console.log("Sin",Math.sin(60))
 
 console.log("cos",Math.cos(0))
 console.log("cos",Math.cos(60))
+
+console.log("String concatenation");
+let hiss = "whys"
+let are = "are" 
+let you = "you'here'"
+let fullName = hiss + are + you; // concatenation, merging two string together.
+console.log("string",fullName);
+
+// Declaring different variables of different data types
+console.log("// Declaring different variables of different data types");
+// 1.Concatenating Using Addition Operator
+console.log("1.Concatenating Using Addition Operator")
+let space = ' '
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let ciy = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let agq = 250
+
+let fulName =firstName + space + lastName
+let personInfoOne = fulName + '\'I am\' ' + agq+ '\" I live in \"' + country; // ES5 string addition
+
+console.log("ADD",personInfoOne)
+
+//2.Long Literal Strings
+console.log("2.Long Literal Strings");
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
+
+console.log("Long Literal",paragraph)
+
+console.log("3.Template Literals (Template Strings)");
+//3.Template Literals (Template Strings)
+console.log(`The sum of 2 and 3 is 5`)              // statically writing the data
+let as = 2
+let bs = 3
+console.log(`The sum of ${as} and ${bs} is ${as + bs}`) // injecting the data dynamically
+
+let f_n  = "Sarthi"
+let ln = "Raval"
+let mn = "ShileshKumar"
+let cy = "Ahmedabad" 
+let js ="JS"
+let jb ='student'
+let ae= 21
+let Fm = f_n +mn+ln
+
+let pikn = `I am ${f_n} ${mn} ${ln} I live in ${cy} .I am ${jb} . I learn ${js}. I am ${ae} years`
+console.log("Template Literals",pikn)
+
+let pin = `I am ${Fm} I live in ${cy} .I am ${jb} . I learn ${js}. I am ${ae} years`
+console.log("Template Literals :- ",pin)
+
+
+console.log("4.Escape Sequences in Strings");
+//4.Escape Sequences in Strings
+console.log("use /n",'I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+console.log("use /t",'Days\tTopics\tExercises')
+console.log("use /t",'Day 1\t3\t5')
+console.log("use /t",'Day 2\t3\t5')
+console.log("use /t",'Day 3\t3\t5')
+console.log("use /t",'Day 4\t3\t5')
+console.log("use \\",'This is a backslash  symbol (\\)') // To write a backslash
+console.log("use \" \"",'In every programming language it starts with \"Hello, World!\"')
+console.log("use \'\'","In every programming language it starts with \'Hello, World!\'")
+console.log("use \'\'",'The saying \'Seeing is Believing\' isn\'t correct in 2020')
+
+
+console.log("String Methods");
+//String Methods
+
+console.log("Length");
+let len = 'I AM DON'
+console.log(len.length)
+
+console.log("Accesing ch in a String");
+//Accesing ch in a String
+let s = "I AM DON"
+console.log("accesnding",s[0])
+console.log("accesnding",s[5])
+console.log("accesnding",s.length -1)
+console.log("accesnding",s[s.length -1])
+
+console.log("uppecase");
+//uppecase
+console.log("uppercase",s.toUpperCase())
+
+
+console.log("lowercase");
+//lowercase
+console.log("lowercase",s.toLowerCase())
+
+console.log("substr");
+//substr()
+let ssr = "JavaScript is Good language" 
+console.log("substr",ssr.substr(0,4))
+console.log("substr",ssr.substr(14))
+console.log("substr","substring and bhai log".substr(1,4))
+
+
+console.log("substring");
+//substring()
+console.log("substring",ssr.substring(0,4))
+console.log("substring",ssr.substring(14))
+console.log("substr","substring and bhai log".substring(1,4))
+
+console.log("split");
+//split
+console.log("split","I am Racer and I Love Racing more than Study".split())
+console.log("split","I am Racer and I Love Racing more than Study".split(''))
+console.log("split","I am Racer and I Love Racing more than Study".split(' '))
+console.log("split",'I, am ,Racer ,and ,I ,Love, Racing ,more ,than ,Study'.split(','))
+
+console.log("trim");
+//trim
+console.log("   I am Racer and I Love Racing more than Study  ".trim())
+
+console.log("Include");
+//include
+let string = '30 Days Of JavaScript'
+console.log("include",string.includes('Days'))     // true
+console.log("include",string.includes('days'))     // false - it is case sensitive!
+console.log("include",string.includes('Script'))   // true
+console.log("include",string.includes('script'))   // false
+console.log("include",string.includes('java'))     // false
+console.log("include",string.includes('Java'))     // true
+
+console.log("replace");
+//replace
+console.log("replace","I am Racer".replace('am Racer','am Good Racer'))
+
+console.log("charAt");
+//charAt()
+console.log("chartat","i am racer".charAt(0))
+let tring = '30 Days Of JavaScript'
+let lastIndex = tring.length - 1
+console.log("charat",tring.charAt(lastIndex)) // t
+
+console.log("chartcodeAt");
+//chartcodeAt()
+console.log("chartcodeAt","i am racer".charCodeAt(0))
+let lastIndexs = string.length - 1
+console.log("chartcodeAt",string.charAt(lastIndexs)) // t
+
+console.log("Indexof");
+//indexof
+let strin = '30 Days Of JavaScript'
+console.log("indexof",strin.indexOf('D'))          // 3
+console.log("indexof",strin.indexOf('Days'))       // 3
+console.log("indexof",strin.indexOf('days'))       // -1
+console.log("indexof",string.indexOf('JavaScript')) // 11
+
+console.log("LastIndex");
+//lastindexof
+let strig = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+
+console.log("lastindexof",strig.lastIndexOf('love'))       // 67
+console.log("lastindexof",strig.lastIndexOf('you'))        // 63
+console.log("lastindexof",strig.lastIndexOf('JavaScript')) //83
+
+console.log("Concat");
+//concat()
+let strng = '30'
+console.log("concat",strng.concat("Days", "Of", "JavaScript")) // 30DaysOfJavaScript
+let coutry = 'Fin'
+console.log("concat",coutry.concat("land")) // Finland
+
+console.log("startwith");
+//startwith()
+let ring = 'Love is the best to in this world'
+console.log("startwith",ring.startsWith('Love'))   // true
+console.log("startwith",ring.startsWith('love'))   // false
+console.log("startwith",ring.startsWith('world'))  // false
+
+console.log("endwith");
+//endwith()
+let ing = 'Love is the most powerful feeling in the world'
+console.log("endwith",ing.endsWith('world'))         // true
+console.log("endwith",ing.endsWith('love'))          // false
+console.log("endwith",ing.endsWith('in the world')) // true
+
+console.log("Search");
+//search()
+let stri = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log("search",stri.search('love'))          // 2
+console.log("search",stri.search(/javascript/gi))  // 7
+
+console.log("Match");
+//match()
+let matchs = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log("match",matchs.match('love'))
+console.log("match",matchs.match(/love/gi))
+
+let txt = 'In 2019, I ran 30 Days of Python. Now, in 2020 I am super exited to start this challenge'
+console.log("match",txt.match(/\d+/g)) // ["2019", "30", "2020"]
+console.log("match",txt.match(/\d+/))  // ["2", "0", "1", "9", "3", "0", "2", "0", "2", "0"]
+
+console.log("Repeat");
+//repeat
+console.log("repeat","love".repeat(5))
